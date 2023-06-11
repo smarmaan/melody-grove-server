e30105e6ead961a5cd9c49632093a013e770a553
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-12 02:10:18 +0600

    booked courses delete specific course api added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 //
 //
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2qbsssi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -114,6 +114,16 @@ async function run() {
       console.log(item);
 
       const result = await bookedCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/booked-courses/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) };
+
+      const result = await bookedCollection.deleteOne(query);
+
       res.send(result);
     });
 
