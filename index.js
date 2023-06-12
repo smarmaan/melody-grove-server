60ee2f7c90971c165a631e3a4203b6fc074392a2
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-12 10:28:56 +0600

    api created to get user data for admin section

--- index.js
+++ index.js
@@ -116,6 +116,21 @@ async function run() {
     });
 
     //   admin related api methods
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+
+      const query = { email: email };
+
+      const user = await usersCollection.findOne(query);
+
+      const result = { admin: user?.role === "admin" };
+
+      res.send(result);
+    });
 
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
