7eb5975b81b959044686d1f8d9709135ba90e4af
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-12 09:47:14 +0600

    jwt api call set up done...

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -54,6 +55,15 @@ async function run() {
     //
     //
     //
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res.send({ token });
+    });
 
     // user related api call methods
 
